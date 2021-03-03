742b01f8cbfe76a632215739caa65b53c4bb0079
andreubita <andre.vaz1411@gmail.com>
2021-03-03 01:39:05 +0000

    POO Fix Ficha1.iEx4: DONE

--- src/2ano/2sem/poo/prt/Fichas/Ficha01/iex4/src/com/andreubita/poo/ficha1/Ex04.java
+++ src/2ano/2sem/poo/prt/Fichas/Ficha01/iex4/src/com/andreubita/poo/ficha1/Ex04.java
@@ -20,5 +20,25 @@ public class Ex04 {
         }
 
         System.out.println("A média das " + n + " temperaturas foi de " + (temps.stream().mapToDouble(a -> a).sum() / n) + " graus");
+        Ex04 ex04 = new Ex04();
+        Tuple<Integer,Integer> biggestVarTuple = ex04.getBiggestVar(temps);
+        double biggestVar = temps.get(biggestVarTuple.y) - temps.get(biggestVarTuple.x);
+        if(biggestVar == 0){
+            System.out.println("A temperatura manteve-se sempre igual");
+        }else{
+            System.out.println("A maior variação registou-se entre os dois " + (biggestVarTuple.x + 1) + " e " + (biggestVarTuple.y + 1)
+                    + ", tendo a temperatura " + (biggestVar > 0 ? "subido ": "descido ") + Math.abs(biggestVar) + " graus");
+        }
+    }
+
+    public Tuple<Integer,Integer> getBiggestVar(ArrayList<Double> temps){
+        int d1 = 0, d2 = 1, b1 = 0, b2 = 0;
+        for(; d2 < temps.size(); d2++, d1++){
+            double temp1 = temps.get(d1), temp2 = temps.get(d2);
+            if(Math.abs(temp1 - temp2) > Math.abs(temps.get(b1) - temps.get(b2))){
+                b1 = d1; b2 = d2;
+            }
+        }
+        return new Tuple<>(b1, b2);
     }
 }
---
+++ src/2ano/2sem/poo/prt/Fichas/Ficha01/iex4/src/com/andreubita/poo/ficha1/Tuple.java
@@ -0,0 +1,10 @@
+package com.andreubita.poo.ficha1;
+
+public class Tuple<X, Y> {
+    public final X x;
+    public final Y y;
+    public Tuple(X x, Y y) {
+        this.x = x;
+        this.y = y;
+    }
+}
