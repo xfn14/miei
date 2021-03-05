93f943ba5e311f734e1d9ff9ced4056ce336b203
andreubita <andre.vaz1411@gmail.com>
2021-03-05 00:28:25 +0000

    POO Fix Ficha1.iEx6: DONE

---
+++ src/2ano/2sem/poo/prt/Fichas/Ficha01/iex6/src/com/andreubita/poo/ficha1/Ex06.java
@@ -0,0 +1,39 @@
+package com.andreubita.poo.ficha1;
+
+import java.util.Scanner;
+
+public class Ex06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Ex06 ex06 = new Ex06();
+        boolean quit = false;
+        while(!quit){
+            System.out.println("Insira o numero (-1 para sair):");
+            int n = scanner.nextInt();
+            if(n != -1){
+                if(n <= 1) System.out.println("Nao existem numeros primos inferiores a " + n);
+                else{
+                    System.out.println("Os numeros primos inferiores a " + n + " sao:");
+                    StringBuilder sb = new StringBuilder();
+                    for(int i = 2; i < n; i++){
+                        if(ex06.isPrime(i))
+                            sb.append(i).append(", ");
+                    }
+                    sb.setLength(sb.length() - 2);
+                    System.out.print(sb.toString() + "\n");
+                }
+            }else{
+                quit = true;
+            }
+        }
+    }
+
+    public boolean isPrime(int n){
+        if(n <= 1) return false;
+
+        for(int i = 2; i < n; i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+}
